5b2cdef7bc561107dd9755c89d547f53da58fa1a
warlyware <danward@gmail.com>
2017-10-16 15:16:31 -0400

    Add games reducer, game list component

---
+++ src/reducers/games.js
@@ -0,0 +1,8 @@
+export default () => {
+  return [
+    { title: 'Punch Out' },
+    { title: 'Metroid' },
+    { title: 'Catherine' },
+    { title: 'Heavy Rain' }
+  ];
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import GamesReducer from './games'
+
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  games: GamesReducer
 });
 
 export default rootReducer;
